6ff6635dee4288e7f5cb97984a8cfd47a8a5dea4
Muhammed Kanyi <kanyi@gcubed.gm>
2025-10-27 03:47:28 +0000

    updated shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :format_options, command_output: true, log_file: "log/capistrano.log", color
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, ".env"
+append :linked_files, ".env", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets",
