f38f9528e64adade0f50675cdea9dc57b0b94370
Muhammed Kanyi <kanyi@gcubed.gm>
2025-11-23 12:22:37 +0000

    adjusted the payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get "pages/index"
   root "pages#index"
+
+  # set root to routers#index if user is authenticated
+  authenticated :user do
+    root to: "routers#index", as: :authenticated_root
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
